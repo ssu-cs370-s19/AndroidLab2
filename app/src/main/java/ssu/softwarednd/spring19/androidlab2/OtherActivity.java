25a4977472ec58e188feff9e769639a4c4a1db68
student <student@ssu-cs-dar28-167-206.sonoma.edu>
2019-02-18 21:53:51 -0800

    work in progress

--- app/src/main/java/ssu/softwarednd/spring19/androidlab2/OtherActivity.java
+++ app/src/main/java/ssu/softwarednd/spring19/androidlab2/OtherActivity.java
@@ -3,19 +3,21 @@ package ssu.softwarednd.spring19.androidlab2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class OtherActivity extends AppCompatActivity {
 
     public static final String NAME_KEY = "USER_NAME";
-    private TextView welcome
+    private EditText welcomeText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        welcomeText = findViewById(R.id.welcome_name_text);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
 
         Intent newIntent = getIntent();
         String name = newIntent.getStringExtra(NAME_KEY);
-        welcomeNameText.setText("Welcome, " + name);
+        welcomeText.setText("Welcome, " + name);
     }
 }
