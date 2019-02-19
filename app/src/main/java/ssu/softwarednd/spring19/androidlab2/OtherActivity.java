5483071bfadece5b47e033010f926ed7ab7e1fb0
student <student@ssu-cs-dar28-167-203.sonoma.edu>
2019-02-18 21:49:30 -0800

    lab 2: working code complete

--- app/src/main/java/ssu/softwarednd/spring19/androidlab2/OtherActivity.java
+++ app/src/main/java/ssu/softwarednd/spring19/androidlab2/OtherActivity.java
@@ -1,13 +1,40 @@
 package ssu.softwarednd.spring19.androidlab2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class OtherActivity extends AppCompatActivity {
+
+    public static final String NAME_KEY = "USER_NAME";
+    private TextView welcomeNameText;
+    private Button returnButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+
+        welcomeNameText = findViewById(R.id.welcome_name_text);
+        returnButton = findViewById(R.id.return_button);
+
+        Intent newIntent = getIntent();
+        String name = newIntent.getStringExtra(NAME_KEY);
+        if (name == null || name.isEmpty()) {
+            name = "Anonymous";
+        }
+        welcomeNameText.setText("Welcome, " + name);
+
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OtherActivity.this, MainActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
