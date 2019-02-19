d80b35911c2c3ecd2594b83c3c40b17c81f029f3
student <student@ssu-cs-dar28-167-212.sonoma.edu>
2019-02-18 19:51:45 -0800

    lab 2: halfway

--- app/src/main/java/ssu/softwarednd/spring19/androidlab2/MainActivity.java
+++ app/src/main/java/ssu/softwarednd/spring19/androidlab2/MainActivity.java
@@ -1,9 +1,15 @@
 package ssu.softwarednd.spring19.androidlab2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button navigateButton;
 
 
     @Override
@@ -11,6 +17,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        navigateButton = findViewById(R.id.navigate_button);
+        navigateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, OtherActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
