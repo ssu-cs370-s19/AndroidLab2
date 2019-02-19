f8da6a5b2efd322264f6de37959208bf1f0f05a6
student <student@ssu-cs-dar28-167-220.sonoma.edu>
2019-02-18 21:54:57 -0800

    work in progress

--- app/src/main/java/ssu/softwarednd/spring19/androidlab2/MainActivity.java
+++ app/src/main/java/ssu/softwarednd/spring19/androidlab2/MainActivity.java
@@ -5,14 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private Button navigateButton;
+    private EditText nameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         navigateButton = findViewById(R.id.navigate_button);
         navigateButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,7 +24,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        nameEditText = findViewById(R.id.name_edit_text);
+        nameEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, OtherActivity.class);
 
+                intent.putExtra(OtherActivity.NAME_KEY, name);
+                startActivity(intent);
+            }
+        });
 
     }
 }
