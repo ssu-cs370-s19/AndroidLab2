63842ec941125a25b219b82167fa437b9ce8c7d7
Colin Sperry <sperryc@sonoma.edu>
2019-02-18 22:04:08 -0800

    lab 2: working code complete

--- app/src/main/java/ssu/softwarednd/spring19/androidlab2/MainActivity.java
+++ app/src/main/java/ssu/softwarednd/spring19/androidlab2/MainActivity.java
@@ -5,11 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private Button navigateButton;
+    private EditText nameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +20,15 @@ public class MainActivity extends AppCompatActivity {
 
         //find view stuff
         navigateButton=findViewById(R.id.navigate_button);
+        nameEditText=findViewById(R.id.name_edit_text);
 
         navigateButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                String name = nameEditText.getText().toString();
                 Intent intent = new Intent(MainActivity.this, OtherActivity.class);
+
+                intent.putExtra(OtherActivity.NAME_KEY, name);
                 startActivity(intent);
             }
         });
--- app/src/main/java/ssu/softwarednd/spring19/androidlab2/OtherActivity.java
+++ app/src/main/java/ssu/softwarednd/spring19/androidlab2/OtherActivity.java
@@ -1,13 +1,44 @@
 package ssu.softwarednd.spring19.androidlab2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class OtherActivity extends AppCompatActivity {
+
+    public static final String NAME_KEY = "USER_NAME";
+    private TextView welcomeNameText;
+    private Button returnToMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+
+        welcomeNameText=findViewById(R.id.welcome_name_text);
+        returnToMain=findViewById(R.id.return_to_main);
+
+        Intent newIntent = getIntent();
+        String name = newIntent.getStringExtra(NAME_KEY);
+
+        if(name == null || name.isEmpty()) {
+            name = "Anonymous";
+        }
+
+        welcomeNameText.setText("Welcome, " + name);
+
+        returnToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OtherActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
