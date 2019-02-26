25a1fd76f02d21a42a3b794a35499c75fd39ebdb
student <student@ssu-cs-dar28-167-201.sonoma.edu>
2019-02-25 21:59:28 -0800

    Lab2: Step 8 done

--- app/src/main/java/ssu/softwarednd/spring19/androidlab2/MainActivity.java
+++ app/src/main/java/ssu/softwarednd/spring19/androidlab2/MainActivity.java
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button navigateButton;
+    private EditText nameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +20,14 @@ public class MainActivity extends AppCompatActivity {
         navigateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
+                String name = nameEditText.getText().toString();
                 Intent intent = new Intent(MainActivity.this, OtherActivity.class);
+                intent.putExtra(OtherActivity.NAME_KEY, name);
                 startActivity(intent);
             }
         });
+        nameEditText = findViewById(R.id.name_edit_text);
+
     }
 }
 
--- app/src/main/java/ssu/softwarednd/spring19/androidlab2/OtherActivity.java
+++ app/src/main/java/ssu/softwarednd/spring19/androidlab2/OtherActivity.java
@@ -2,12 +2,19 @@ package ssu.softwarednd.spring19.androidlab2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.TextView;
 
 public class OtherActivity extends AppCompatActivity {
 
-    @Override
+    public static final String NAME_KEY = "USER_NAME";
+    private TextView welcomeNameText;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+        welcomeNameText = findViewById(R.id.welcome_name_text);
+        Intent newIntent = getIntent();
+        String name = newIntent.getStringExtra(NAME_KEY);
+        welcomeNameText.setText("Welcome, " + name);
     }
 }
